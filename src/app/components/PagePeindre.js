5171ecf55179bc65bc559153b0261a643926c3ea
alexisben <alex@noesya.coop>
2023-02-10 11:56:23 +0100

    add dynamic import to fix P5 react wrapper

--- src/app/components/PagePeindre.js
+++ src/app/components/PagePeindre.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { ReactP5Wrapper } from 'react-p5-wrapper'
+import dynamic from 'next/dynamic'
 import sketch from './sketch'
 import styles from '../styles/components/PagePeindre.module.css'
+
+const ReactP5Wrapper = dynamic(() => import('react-p5-wrapper')
+    .then(mod => mod.ReactP5Wrapper), {
+        ssr: false
+    })
 
 function PagePeindre() {
     return (
         <>
             <div className={styles.content} >
                 <div className={styles.drawboard } id='test'>
-                     <ReactP5Wrapper sketch={sketch} /> 
+                    <ReactP5Wrapper sketch={sketch} /> 
                 </div>
                 <button className={styles.buttonValider}>
                     Valider
