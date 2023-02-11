c7df65b8ccac672ac10dac4c2af1ee7f81ca036d
ODrimm <ystos.gaming@gmail.com>
2023-02-11 13:09:42 +0100

    acceuil v1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,46 @@
 import Choix from '../app/components/Choix'
+import '../app/styles/homePage.css'
 
 function index(){
 
     return(
-        <>
-            <h1>Menu</h1>
-            <Choix shown="true" contenus="Jouer/Crédits" liens={[
-            { href: "/chapitres/intro/1", as: "/chapitres/intro/1" },
-            { href: "/credits" },
-            ]}></Choix>
+        <>  
+            <section className='page'>
+                
+
+                <section className='logo'>
+                    <section className='topLogo'> 
+                        <img className='textLogo' src="/AvaLogo.svg" alt="Logo Ava" />
+                        <div className='circle'></div>
+                    </section>
+                    <section className='bottomLogo'>
+                        <h2 className='subTextLogo'>Ad Vitam Aeternam</h2>
+                        <img className='logoDeco' src="/AvaLogoDeco.svg" alt="Logo Ava" />
+                    </section>
+
+                    
+
+                </section>
+
+
+
+                <section className='boutons'>
+                    <Choix shown="true" contenus="Jouer" liens={[
+                    { href: "/chapitres/intro/1", as: "/chapitres/intro/1" },
+                    ]}></Choix>
+
+                    <Choix shown="true" contenus="Crédits" liens={[
+                    { href: "/credits" },
+                    ]}></Choix>
+
+                </section>
+
+                <section className='decoBottom'>
+                    <img src='/menuBottomDeco.svg'></img>
+                </section>
+                
+            </section>
+            
         </>
 
     )
