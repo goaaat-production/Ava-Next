8d48f66ce611ae4dbf35bb9dbe4b1af710633e16
Killian JF <killian.jaffrelot@mmibordeaux.com>
2023-02-12 20:19:35 +0100

    changes dnd

--- src/app/components/dragAndDrop.js
+++ src/app/components/dragAndDrop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router'
 import ItemsDND from './itemsDND';
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useDrop } from 'react-dnd';
 import { useDrag } from 'react-dnd';
 import styles from '../styles/components/dragAndDrop.module.css'
@@ -52,25 +52,26 @@ var globalItemId = null
 
 var DialogueCap = " :Capitaine : Place les objets au bon endroit !"
 
-export const generatePreview = ({itemType, item, style}) => {
-    return <div  className={styles.preview} style={style}><img src={baseItemList[item.id].url}></img></div>
+export const generatePreview = ({ itemType, item, style }) => {
+    return <div className={styles.preview} style={style}><img src={baseItemList[item.id].url}></img></div>
 }
 
 
 
 function removeObjectWithId(arr, id) {
     const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
-  
-    if (objWithIdIndex > -1) {
-      arr.splice(objWithIdIndex, 1);
-    }
-  
-    return arr;
-  }
-  
 
-function IsFinish (array) {
-    if (array.length===1){
+    if (objWithIdIndex > -1) {
+        arr.splice(objWithIdIndex, 1);
+    }
+
+    return arr;
+}
+
+
+function IsFinish(array) {
+    if (array.length === 1) {
+        console.log(array.length===1)
         return true
     }
 }
@@ -81,41 +82,41 @@ function DragAndDrop() {
     const router = useRouter()
     let [isLastPhrase, setIsLastPhrase] = useState(false);
 
-    const gagné = () =>{
+    const gagné = () => {
         console.log('Vous avez gagné')
         // router.push('./3')    
     }
 
 
-    function DialogueCapitaine(itemId,boardId) {
+    function DialogueCapitaine(itemId, boardId) {
         if (itemId === 2 & boardId === 1) {
-            return(
-            ":Capitaine : Oui le siège de mozart fait un parfait contrepoid pour redresser le navire. ")
+            return (
+                ":Capitaine : Oui le siège de mozart fait un parfait contrepoid pour redresser le navire. ")
         }
         else if (itemId === 0 & boardId === 2) {
-            return(
-            ":Capitaine : Exactement ! Le ruban va pouvoir récuperer les gens à la mer !")
+            return (
+                ":Capitaine : Exactement ! Le ruban va pouvoir récuperer les gens à la mer !")
         }
         else if (itemId === 1 & boardId === 3) {
-            return(
-            ":Capitaine : Parfait ! On va pouvoir repeindre sur les flammes.")
+            return (
+                ":Capitaine : Parfait ! On va pouvoir repeindre sur les flammes.")
         }
 
 
         else if (itemId === 2 & boardId !== 1) {
-            return(
-            ":Capitaine : Noon ! On a besoin de poid à tribord !  ")
+            return (
+                ":Capitaine : Noon ! On a besoin de poid à tribord !  ")
         }
         else if (itemId === 1 & boardId !== 3) {
-            return(
-            ":Capitaine : Pas ici ! Le pinceau peut nous aider à éteindre les flammes... ")
+            return (
+                ":Capitaine : Pas ici ! Le pinceau peut nous aider à éteindre les flammes... ")
         }
-        else if (itemId === 0 & boardId !== 2){
-            return(
-            ":Capitaine : Non pas là ! Le ruban doit pouvoir récuperer des gens")
-        }        
+        else if (itemId === 0 & boardId !== 2) {
+            return (
+                ":Capitaine : Non pas là ! Le ruban doit pouvoir récuperer des gens")
+        }
     }
-    
+
 
 
 
@@ -150,7 +151,7 @@ function DragAndDrop() {
         collect: (monitor) => ({
             isOver3: !!monitor.isOver(),
         })
-    }))    
+    }))
 
 
     const AddImageToBoard = (itemId, boardId) => {
@@ -159,53 +160,53 @@ function DragAndDrop() {
             setBoard((board) => [...board, pictureList[0]]);
             removeObjectWithId(ItemList, itemId)
             NombreObjet += 1;
-            DialogueCap = DialogueCapitaine(2,1)
+            DialogueCap = DialogueCapitaine(2, 1)
         }
         else if (itemId === 0 & boardId === 2) {
             setBoard2((board2) => [...board2, pictureList[0]]);
             removeObjectWithId(ItemList, itemId)
             NombreObjet += 1;
-            DialogueCap = DialogueCapitaine(0,2)
+            DialogueCap = DialogueCapitaine(0, 2)
         }
         else if (itemId === 1 & boardId === 3) {
             setBoard3((board3) => [...board3, pictureList[0]]);
             removeObjectWithId(ItemList, itemId)
             NombreObjet += 1;
-            DialogueCap = DialogueCapitaine(1,3)
+            DialogueCap = DialogueCapitaine(1, 3)
         }
         else if (itemId === 2 & boardId !== 1) {
 
-            
-            if (boardId===2) {
-                Board2reussi =false
-                DialogueCap = DialogueCapitaine(2,1)
+
+            if (boardId === 2) {
+                Board2reussi = false
+                DialogueCap = DialogueCapitaine(2, 1)
             }
-            else if (boardId===3) {
-                Board3reussi =false
-                DialogueCap = DialogueCapitaine(2,3)
+            else if (boardId === 3) {
+                Board3reussi = false
+                DialogueCap = DialogueCapitaine(2, 3)
             }
         }
         else if (itemId === 1 & boardId !== 3) {
-                        if (boardId===2) {
-                Board2reussi =false
-                DialogueCap = DialogueCapitaine(1,2)
+            if (boardId === 2) {
+                Board2reussi = false
+                DialogueCap = DialogueCapitaine(1, 2)
             }
-            else if (boardId===1) {
-                Board1reussi =false
-                DialogueCap = DialogueCapitaine(1,1)
+            else if (boardId === 1) {
+                Board1reussi = false
+                DialogueCap = DialogueCapitaine(1, 1)
             }
 
         }
         else if (itemId === 0 & boardId !== 2) {
 
 
-            if (boardId===1) {
-                Board1reussi =false
-                DialogueCap = DialogueCapitaine(0,1)
+            if (boardId === 1) {
+                Board1reussi = false
+                DialogueCap = DialogueCapitaine(0, 1)
             }
-            else if (boardId===3) {
-                Board3reussi =false
-                DialogueCap = DialogueCapitaine(0,3)
+            else if (boardId === 3) {
+                Board3reussi = false
+                DialogueCap = DialogueCapitaine(0, 3)
             }
         }
     }
@@ -214,58 +215,61 @@ function DragAndDrop() {
     const toggle = () => {
         this.setState(prev => ({ reveal: !prev.reveal }))
     }
- 
+
 
 
     return (
-        <> 
-        <Preview generator={generatePreview}/>
+        <>
+            <Preview generator={generatePreview} />
             <div className={styles.content}>
+               
                 <div className={styles.boards}>
-                    <img src='/lincendie-du-steamer-austria.png'></img>
-                        <div className={styles.boardSlots}> 
-                            <div className = {styles.boardSlot}>
-                                <div id= "board1" className={`${styles.board1} ${(!Board1reussi? styles.Shaking : '')}`} ref={drop} style={{
-                                    backgroundColor: isOver? "yellow" : "white",
-                                    width: isOver? "25vw":"15vw",
-                                    height: isOver? "25vw":"15vw",
-                                    opacity: IsFinish(board)? '0%' : '40%'
-                                }} >   
+                        <img src='/lincendie-du-steamer-austria.png'></img>
+                        <div className={styles.boardSlots}>
+                            <div className={styles.boardSlot1} ref={drop}>
+                                <div id="board1" className={`${styles.board1} ${(!Board1reussi ? styles.Shaking : '')}`}  style={{
+                                    backgroundColor: isOver ? "rgba(255,255,255,100%)" : "rgba(0,0,0,0)",
+                                    width: isOver ? "30vw" : "15vw",
+                                    height: isOver ? "30vw" : "15vw",
+                                    opacity: IsFinish(board)? '0%' : '100%'
+                                }} >
                                     {board.map((item) => {
                                         return <ItemsDND key={item.id} url={item.url} id={item.id} />
                                     })}
                                 </div>
                             </div>
-                            <div className = {styles.boardSlot}>
-                                <div id= "board2" className={`${styles.board2} ${(!Board2reussi? styles.Shaking : '')}`} ref={drop2} style={{
-                                    backgroundColor: isOver2? "yellow" : "white",
-                                    width: isOver2? "25vw":"15vw",
-                                    height: isOver2? "25vw":"15vw",
-                                    opacity: IsFinish(board2)? '0%' : '40%'
-                                }} >   
+                            <div className={styles.boardSlot2} ref={drop2}>
+                                <div id="board2" className={`${styles.board2} ${(!Board2reussi ? styles.Shaking : '')}`} style={{
+                                    backgroundColor: isOver2 ? "rgba(255,255,255,100%)" : "rgba(0,0,0,0)",
+                                    width: isOver2 ? "30vw" : "15vw",
+                                    height: isOver2 ? "30vw" : "15vw",
+                                    opacity: IsFinish(board2) ? '0%' : '100%'
+                                }} >
                                     {board2.map((item) => {
                                         return <ItemsDND key={item.id} url={item.url} id={item.id} />
                                     })}
                                 </div>
                             </div>
-                            <div className = {styles.boardSlot}>
-                                <div id= "board3" className={`${styles.board3} ${(!Board3reussi? styles.Shaking : '')}`} ref={drop3} style={{
-                                    backgroundColor: isOver3? "yellow" : "white",
-                                    width: isOver3? "25vw":"15vw",
-                                    height: isOver3? "25vw":"15vw",
-                                    opacity: IsFinish(board3)? '0%' : '40%'
-                                }} >   
-                                
+                            <div className={styles.boardSlot3} ref={drop3}>
+                                <div id="board3" className={`${styles.board3} ${(!Board3reussi ? styles.Shaking : '')}`} style={{
+                                    backgroundColor: isOver3 ? "rgba(255,255,255,100%)" : "rgba(0,0,0,0)",
+                                    width: isOver3 ? "30vw" : "15vw",
+                                    height: isOver3 ? "30vw" : "15vw",
+                                    opacity: IsFinish(board3) ? '0%' : '100%'
+                                }} >
+
                                     {board3.map((item) => {
-                                        return <ItemsDND  key={item.id} url={item.url} id={item.id} />
+                                        return <ItemsDND key={item.id} url={item.url} id={item.id} />
                                     })}
                                 </div>
                             </div>
                         </div>
-                    <div className={styles.dialogue}>
-                        {<Dialogue  setIsLastPhrase={setIsLastPhrase}   dialogue= {DialogueCap}></Dialogue>}
-                    </div>
+                        <div className={styles.dialogue}>
+                        {<Dialogue setIsLastPhrase={setIsLastPhrase} dialogue={DialogueCap}></Dialogue>}
+                        </div>
+                    
                 </div>
+                
                 <div className={(ItemList.length >= 1 ? `${styles.items}` : `${styles.items} ${styles.hidden}`)}>
                     {ItemList.map((item) => {
                         return <ItemsDND key={item.id} url={item.url} id={item.id} />
@@ -274,8 +278,8 @@ function DragAndDrop() {
                 <Choix shown={ItemList.length <= 0}
                     contenus="Vous êtes sauvés !"
                     liens={[
-                        { href: "./3", as: "./3" },                        
-                        ]}                                
+                        { href: "./3", as: "./3" },
+                    ]}
                 ></Choix>
 
                 <div>
@@ -283,7 +287,7 @@ function DragAndDrop() {
                         (NombreObjet === 3 ? gagné() : null)
                     }
                 </div>
-                
+
             </div>
 
 
