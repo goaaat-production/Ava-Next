0855c04c483cea6cf345a92bd3758fbc04210010
ODrimm <ystos.gaming@gmail.com>
2023-03-09 18:27:52 +0100

    credit fini?

--- src/pages/credits.jsx
+++ src/pages/credits.jsx
@@ -31,44 +31,20 @@ function Credits() {
                         <section className='backGround'>
                         </section>
 
-                        <h3>ARC Production</h3>
-                        <h3>MMI Bordeaux Montaigne & <br /> Musée des Beaux-Arts</h3>
+                        <h3>Un projet de : <br /> ARC Production</h3>
 
                         <section className='creditPart'>
-                            <h4>Chef de projet</h4>
-                            <p>
-                                Camille Fourcade
-                            </p>
-                        </section>
-                        <section className='creditPart'>
-                            <h4>Conception & Rédaction</h4>
-                            <p>
-                                ARC Production
-                            </p>
-                        </section>
-                        <section className='creditPart'>
-                            <h4>Direction Artistique & <br />Design d’interface</h4>
+                            <h4>L'équipe :</h4>
                             <p>
                                 Cléo Laffargue <br />
                                 Perrine Joly <br />
-                                Camille Fourcade
+                                Camille Fourcade <br />
+                                Francia Bidzimou <br />
+                                Killian Jaffrelot <br />
+                                Anaïs Delavier
                             </p>
                         </section>
-                        <section className='creditPart'>
-                            <h4>Développement</h4>
-                            <p>
-                                Francia Bidzimou<br />
-                                Killian Jaffrelot<br />
-                                Camille Fourcade
-                            </p>
-                        </section>
-                        <section className='creditPart'>
-                            <h4>Sound Design</h4>
-                            <p>
-                                Anaïs Delavier<br />
-                                Cléo Laffargue
-                            </p>
-                        </section>
+                        
 
                     </section>
                     :
@@ -76,28 +52,28 @@ function Credits() {
                         <section className='backGround'>
                         </section>
 
-                        <h3>ARC Production</h3>
-                        <h3>MMI Bordeaux Montaigne & <br /> Musée des Beaux-Arts</h3>
+                        <h3>MMI Bordeaux Montaigne &</h3>
+                        <section className='musba'></section>
 
                         <section className='creditPart'>
                             <h4>Tableaux utilisés : </h4>
                             <p>
-                                “L'INCENDIE DU STEAMER AUSTRIA” <br/> Eugène Isabey - 1858
+                                “L'INCENDIE DU STEAMER AUSTRIA” <br /> Eugène Isabey - 1858
                             </p>
                         </section>
                         <section className='creditPart'>
                             <p>
-                                “EMBARQUEMENT DE LA DUCHESSE D’ANGOULÊME À PAUILLAC” <br/> Antoine Jean Gros - 1818
+                                “EMBARQUEMENT DE LA DUCHESSE D’ANGOULÊME À PAUILLAC” <br /> Antoine Jean Gros - 1818
                             </p>
                         </section>
                         <section className='creditPart'>
                             <p>
-                                “MOZART EXPIRANT” <br/> Rinaldo Carnielo - 1877/1880
+                                “MOZART EXPIRANT” <br /> Rinaldo Carnielo - 1877/1880
                             </p>
                         </section>
                         <section className='creditPart'>
                             <p>
-                                “CHARLES QUINT RAMASSANT LE PINCEAU DE TITIEN” <br/> Pierre-Nolasque Bergeret - 1808
+                                “CHARLES QUINT RAMASSANT LE PINCEAU DE TITIEN” <br /> Pierre-Nolasque Bergeret - 1808
                             </p>
                         </section>
                     </section>
